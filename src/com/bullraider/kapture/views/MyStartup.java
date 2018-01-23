1383f7ac6a27d9cfc797790632cf28f84b5d77ac
Ab670958 <abhiram.mishra@wipro.com>
2018-01-24 00:14:58 +0530

    Added keyboard recognizer.

--- src/com/bullraider/kapture/views/MyStartup.java
+++ src/com/bullraider/kapture/views/MyStartup.java
@@ -1,13 +1,7 @@
 package com.bullraider.kapture.views;
 
-import java.awt.Event;
-
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Listener;
 import org.eclipse.ui.IStartup;
 import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 
 public class MyStartup implements IStartup {
@@ -15,20 +9,20 @@ public class MyStartup implements IStartup {
 	@Override
 	public void earlyStartup() {
 		final IWorkbench workbench = PlatformUI.getWorkbench();
-		Display display = workbench.getDisplay();
-		display.asyncExec(new Runnable() {
-			public void run() {
-				IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
-				if (window != null) {
-					display.addFilter(SWT.KeyUp, new Listener() {
-						@Override
-						public void handleEvent(org.eclipse.swt.widgets.Event event) {
-							System.out.println("Something azmaing"+event.character+" "+ event.keyCode+" "+event.keyLocation);
-						}
-					}); 
-				}
-			}
-		});
+//		Display display = workbench.getDisplay();
+//		display.asyncExec(new Runnable() {
+//			public void run() {
+//				IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
+//				if (window != null) {
+//					display.addFilter(SWT.KeyUp, new Listener() {
+//						@Override
+//						public void handleEvent(org.eclipse.swt.widgets.Event event) {
+//							System.out.println("Something azmaing"+event.character+" "+ event.keyCode+" "+event.keyLocation+" "+event.character);
+//						}
+//					}); 
+//				}
+//			}
+//		});
 
 	}
 
--- src/com/bullraider/kapture/views/ShortCutCapture.java
+++ src/com/bullraider/kapture/views/ShortCutCapture.java
@@ -2,30 +2,37 @@ package com.bullraider.kapture.views;
 
 
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
+
+import org.eclipse.swt.widgets.Event;
+
 import org.eclipse.ui.part.*;
-import org.eclipse.jface.viewers.*;
-import org.eclipse.swt.graphics.Color;
+
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.FontData;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.jface.action.*;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.text.TextViewer;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+import org.eclipse.core.commands.IExecutionListener;
+
+import org.eclipse.core.commands.NotHandledException;
+
+import org.eclipse.jface.bindings.keys.KeySequence;
+import org.eclipse.jface.bindings.keys.ParseException;
+
 import org.eclipse.ui.*;
-import org.eclipse.swt.widgets.Menu;
+
+import org.eclipse.ui.commands.ICommandService;
+import org.eclipse.ui.internal.keys.WorkbenchKeyboard;
+
+
+
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.ControlEvent;
-import org.eclipse.swt.events.ControlListener;
-
 import javax.inject.Inject;
-import javax.swing.text.LabelView;
 
-import org.eclipse.jface.viewers.StyledString;
 
 
 /**
@@ -46,6 +53,7 @@ import org.eclipse.jface.viewers.StyledString;
  * <p>
  */
 
+@SuppressWarnings("restriction")
 public class ShortCutCapture extends ViewPart {
 
 	/**
@@ -56,23 +64,141 @@ public class ShortCutCapture extends ViewPart {
 	@Inject IWorkbench workbench;
 	private Text text;
 
+	
 	@Override
 	public void createPartControl(Composite parent) {
 		text = new Text(parent, SWT.NONE);
-		parent.setLayout( new GridLayout( 1, false ) );
+		
 		GridData data = new GridData(SWT.CENTER, SWT.CENTER, true, true);
 		Font boldFont = new Font( text.getDisplay(), new FontData( "Arial", 45, SWT.BOLD |SWT.CENTER) );
 		text.setFont( boldFont );
-		text.setText("Bold Label");
-		data.heightHint=95;
-		text.setLayoutData(data);
-		
 	
+		text.setEditable(false);
+		text.setText("");
+		data.heightHint=95;
+		data.widthHint=295;
+		text.setLayoutData(data);
+		parent.setLayout( new GridLayout( 1, false ) );
+		
+//		IBindingService srv = (IBindingService) PlatformUI.getWorkbench().getService(IBindingService.class);
+//		Binding[] bindings = srv.getBindings();
+//		  BindingService bindingService = (BindingService) PlatformUI.getWorkbench().getAdapter(IBindingService.class);
+		ICommandService command = PlatformUI.getWorkbench().getAdapter(ICommandService.class);
+		command.addExecutionListener(new  IExecutionListener() {
+			
+			@Override
+			public void preExecute(String arg0, ExecutionEvent arg1) {
+				Event e=(Event)arg1.getTrigger();
+				String format="";
+				try {
+					
+					format=KeySequence.getInstance(WorkbenchKeyboard.generatePossibleKeyStrokes(e).get(0).toString()).format();
+				} catch (ParseException e1) {
+					// TODO Auto-generated catch block
+					format=WorkbenchKeyboard.generatePossibleKeyStrokes(e).get(0).toString();
+					e1.printStackTrace();
+				}
+			
+				text.setText(format);
+				
+			}
+			
+			@Override
+			public void postExecuteSuccess(String arg0, Object arg1) {
+			//NO op
+			}
+			
+			@Override
+			public void postExecuteFailure(String arg0, ExecutionException arg1) {
+				//NO op
+			}
+			
+			@Override
+			public void notHandled(String arg0, NotHandledException arg1) {
+				//NO op
+			}
+		});
+		
+		//srv.getPerfectMatch(new )
+//		srv.addBindingManagerListener(new IBindingManagerListener() {
+//			
+//			@Override
+//			public void bindingManagerChanged(BindingManagerEvent arg0) {
+//				// TODO Auto-generated method stub
+//					arg0.
+//				
+//			}
+//		});
+//		srv.
+//		srv.setKeyFilterEnabled(false);
+	//	Display display = workbench.getDisplay();
+//		
+//		workbench.getActiveWorkbenchWindow().getShell().addKeyListener(new org.eclipse.swt.events.KeyListener() {
+//			
+//			@Override
+//			public void keyReleased(org.eclipse.swt.events.KeyEvent arg0) {
+//				 int accelerator = SWTKeySupport.convertEventToUnmodifiedAccelerator(arg0);
+//	                KeySequence sequence = KeySequence.getInstance((KeyStroke)arg0.getSource());
+//	                System.out.println("sdf");
+//			}
+//			
+//			@Override
+//			public void keyPressed(org.eclipse.swt.events.KeyEvent arg0) {
+//				 int accelerator = SWTKeySupport.convertEventToUnmodifiedAccelerator(arg0);
+//	                KeySequence sequence = KeySequence.getInstance((KeyStroke)arg0.getSource());
+//	                System.out.println("sdf");
+//			}
+//		});
+//		
+//		display.asyncExec(new Runnable() {
+//			
+//			public void run() {
+//				System.out.println(this);
+//				IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
+//				if (window != null) {
+//					
+//					display.addFilter(SWT.KeyUp , new Listener() {
+//						String s="";
+//						@Override
+//						public void handleEvent(org.eclipse.swt.widgets.Event event) {
+//							List generatePossibleKeyStrokes = WorkbenchKeyboard.generatePossibleKeyStrokes(event);
+//							
+//							System.out.println("Something azmaing  Threadid: "+ event.keyCode+" "+event.keyLocation+" "+event.character+"   "+event.stateMask);
+//							char c =event.character;
+//							if((event.stateMask & SWT.CTRL )!=0) {
+//								s="⌘ +"+c;
+//							}
+//							if((event.stateMask & SWT.CTRL)!=0) {
+//								s="⌃ + "+c;
+//							}
+//							if((event.stateMask & SWT.COMMAND)!=0) {
+//								s="⌘ + "+c;
+//							}
+//							if((event.stateMask & SWT.SHIFT)!=0) {
+//								s="⇧ + "+c;
+//							}
+//							if((event.stateMask & SWT.ALT)!=0) {
+//								s="⌥ + "+c;
+//							}
+//							if((event.stateMask & SWT.ALT)!=0 && (event.stateMask & SWT.COMMAND)!=0)  {
+//								s="⌥ + ⌘ +"+c;
+//							}
+//							
+//						//	text.setText(generatePossibleKeyStrokes.toString());
+//							
+//							//for(int i=0;i<255;i++) {
+//							//text.setBack(display.getSystemColor(44));
+//							//}
+//						}	
+//					}); 
+//				}
+//			}
+//		});
+//		System.out.println(getSite().getKeyBindingService().getScopes());
 	}
 
 	@Override
 	public void setFocus() {
 		// TODO Auto-generated method stub
-		
 	}
 }
